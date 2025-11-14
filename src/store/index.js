3caeb9edaa473706d873b2e7e533d39d591694ff
Steve Tyler <steve.tyler75@gmail.com>
2025-11-14 19:50:47 +0000

    added slice and removed old reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,29 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-    if(action.type === 'increment') {
-        // Always return a new state object
-        // Never mutate the existing state
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++; // Immer library used under the hood to allow direct state mutation
+        },
+        increase(state, action) {
+            state.counter += action.payload;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
         }
     }
-    if(action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
+})
 
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-
-    return state;
-}
-
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
 export default store;
 
